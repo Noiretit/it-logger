1bde0d9dc84d9c0ad9f1170595937b264decf47f
Noiret <sergio.ko@icloud.com>
2020-10-08 13:13:08 +0200

    Created a TechSelectOption component in order to bring technicians from the server and imported them on Add and Edit LogModal's

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import TechSelectOption from '../techs/TechSelectOptions';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types'
 import { addLog } from '../../actions/logActions';
@@ -61,9 +62,7 @@ const AddLogModal = ({ addLog }) => {
                             onChange={e => setTech(e.target.value)}
                         >
                             <option value="" disabled>Select technician</option>
-                            <option value="John Doe">John Doe</option>
-                            <option value="Sam Smith">Sam Smith</option>
-                            <option value="Sara Wilson">Sara Wilson</option>
+                            <TechSelectOption />
                         </select>
                     </div>
                 </div>

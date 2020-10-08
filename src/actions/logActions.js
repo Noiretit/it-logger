6caa0f4918e3d23169832d8c6b98476eb78c9716
Noiret <sergio.ko@icloud.com>
2020-10-08 11:54:06 +0200

    User can search for specific logs: created searchLogs action in logActions.js (hits and endpoint, so the filtered information already arrives from the server, no need to gather all logs and filter them in our app); passed it to the reducer which returns the actual log; connected SearchBar.js with redux, brought searchLogs action and passed it as props to the searchbar (as well as at the export); added a placeholder to the bar. Imported useRef to get the value from the form: stored the hook in a variable (text) which is added into the input <ref={text}> and added an onChange event to set the input to searchLogs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -5,7 +5,9 @@ import {
     ADD_LOG, 
     DELETE_LOG, 
     SET_CURRENT, 
-    CLEAR_CURRENT, UPDATE_LOG
+    CLEAR_CURRENT, 
+    UPDATE_LOG,
+    SEARCH_LOGS
 } from './types';
 
 
@@ -102,6 +104,25 @@ export const updateLog = (log) => async dispatch =>{
 
         dispatch({
             type: UPDATE_LOG,
+            payload: data
+        });
+    } catch (err) {
+        dispatch({
+            type: LOGS_ERROR,
+            payload: err.response.data
+        })
+    }
+};
+
+// Search server logs
+export const searchLogs = (text) => async dispatch =>{
+    setLoading();
+    try {
+        const res = await fetch(`/logs?q=${text}`);
+        const data = await res.json();
+
+        dispatch({
+            type: SEARCH_LOGS,
             payload: data
         });
     } catch (err) {
--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -6,7 +6,8 @@ import {
     DELETE_LOG,
     UPDATE_LOG,
     SET_CURRENT,
-    CLEAR_CURRENT
+    CLEAR_CURRENT,
+    SEARCH_LOGS
 } from '../actions/types';
 
 const initialState = {
@@ -43,6 +44,11 @@ export default(state = initialState, action) => {
                 // a match with the id of the log we want to update. If it is, return the updated log
                 // If not, return whatever log is in state
                 logs: state.logs.map(log => log.id === action.payload.id ? action.payload : log)
+            };
+        case SEARCH_LOGS:
+            return {
+                ...state,
+                logs: action.payload
             }
         case SET_CURRENT:
             return {

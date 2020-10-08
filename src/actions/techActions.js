f8d15943f5e5e8251251ab416ae63a13ed6407e6
Noiret <sergio.ko@icloud.com>
2020-10-08 15:06:09 +0200

    User can delete a technician: created deleteTech action in techActions, passed it to the reducer, connected TechItem.js component to redux, passed deleteTech action and bringed M to show a message. Destructured tech state and passed deleteTech as props

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -50,6 +50,26 @@ export const addTech = (tech) => async dispatch =>{
     }
 };
 
+// Delete a technician
+export const deleteTech = (id) => async dispatch =>{
+    setLoading();
+    try {
+        await fetch(`/techs/${id}`, {
+            method: 'DELETE'
+        });
+
+        dispatch({
+            type: DELETE_TECH,
+            payload: id
+        });
+    } catch (err) {
+        dispatch({
+            type: TECHS_ERROR,
+            payload: err.response.statusText
+        })
+    }
+};
+
 // Set loading to true
 export const setLoading = () => {
     return {
--- src/components/techs/TechItem.js
+++ src/components/techs/TechItem.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types'
+import { deleteTech } from '../../actions/techActions';
+import M from 'materialize-css/dist/js/materialize.min.js';
 
-const TechItem = ({ tech }) => {
+
+const TechItem = ({ tech: {firstName, lastName, id}, deleteTech }) => {
+    const onDelete = () => {
+        deleteTech(id);
+        M.toast({ html: `Technician "${firstName} ${lastName}" has been deleted` })
+    }
     return (
         <li className="collection-item">
             <div>
-                {tech.firstName} {tech.lastName}
+                {firstName} {lastName}
                 {/* secondary-content moves it to the right of the item */}
-                <a href="#!" className="secondary-content">
+                <a href="#!" onClick={onDelete} className="secondary-content">
                     <i className="material-icons grey-text">delete</i>
                 </a>
             </div>
@@ -17,6 +25,7 @@ const TechItem = ({ tech }) => {
 
 TechItem.propTypes = {
     tech: PropTypes.object.isRequired,
+    deleteTech: PropTypes.func.isRequired,
 };
 
-export default TechItem
+export default connect(null, { deleteTech })(TechItem);

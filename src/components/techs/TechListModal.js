275b65ca31719b0da24adf2bc08a11dab0760be4
Noiret <sergio.ko@icloud.com>
2020-10-08 13:02:40 +0200

    Tech list is now feteched from server: created techActions.js and added getTechs (calls the /techs endpoint), sends it to techReducer (which is imported to index.js inside src/reducers), connect TechListModal with redux, import getTechs action, pass the action and techs+loading (from techReducer, which are brought with mapStateToProps at the export of TechListModal). useState is commented out bc there is an upper state (the whole point of redux) as well as the getTechs from this component

--- src/components/techs/TechListModal.js
+++ src/components/techs/TechListModal.js
@@ -1,9 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getTechs } from '../../actions/techActions'
+
 import TechItem from './TechItem'
 
-const TechListModal = () => {
-    const [techs, setTechs] = useState([]);
-    const [loading, setLoading] = useState(false);
+const TechListModal = ({ getTechs, tech: { techs, loading } }) => {
+    // const [techs, setTechs] = useState([]);
+    // const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         getTechs();
@@ -11,27 +15,39 @@ const TechListModal = () => {
     }, []);
     //We add an empty array to it only runs ONCE
 
-    const getTechs = async () => {
-        setLoading(true);
-        const res = await fetch('/techs');
-        const data = await res.json();
+    // const getTechs = async () => {
+    //     setLoading(true);
+    //     const res = await fetch('/techs');
+    //     const data = await res.json();
 
-        setTechs(data);
-        setLoading(false);
-    };
+    //     setTechs(data);
+    //     setLoading(false);
+    // };
 
     return (
         <div id="tech-list-modal" className="modal">
             <div className="modal-content">
                 <h4>Technician list</h4>
                 <ul className="collection">
-                    {!loading && techs.map(tech => (
-                        <TechItem tech={tech} key={tech.id}/>
+                    {!loading && 
+                        techs !== null && 
+                        techs.map(tech => (
+                            <TechItem tech={tech} key={tech.id}/>
                     ))}
                 </ul>
             </div>
         </div>
     )
+};
+
+TechListModal.propTypes = {
+    tech: PropTypes.object.isRequired,
+    getTechs: PropTypes.func.isRequired
 }
 
-export default TechListModal;
+const mapStateToProps = state => ({
+    // [nombre del prop]: state.[nombre del reducer en index.js]
+    tech: state.tech
+})
+
+export default connect(mapStateToProps, { getTechs })(TechListModal);
